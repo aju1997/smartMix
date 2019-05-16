a2b37244d75393fe1479dda43d79dd6cb2f1dee2
Anderson Ju <aju001@ucr.edu>
2019-05-16 12:08:46 -0700

    Added simplified if statements for custom drinks

--- arduino_code/main/main.ino
+++ arduino_code/main/main.ino
@@ -36,249 +36,77 @@ void loop()
   {
     input = EEBlue.readString(); //Read the incoming data and store it into variable data
     
-    input.toCharArray(data,4);
-//    Serial.print(input);
-//    Serial.print(data);
-//    input = EEBlue.readString();
-//    Serial.print(data);
+    input.toCharArray(data,4);\
     
     first = data[0] - '0';
     second = data[1] - '0';
     third = data[2] - '0';
-//    Serial.print(data[0]);
-//    Serial.print(data[1]);
-//    Serial.print(data[2]);
-//    Serial.print("data");
-//    Serial.print(first);
-//    Serial.print(second);
-//    Serial.print(third);
-//    Serial.print("end");
-    if (first > 0 && second > 0 && third > 0) { // output from all 3 drinks
-      Serial.print("Drink 1, 2, 3\n");
-      //change
-      digitalWrite(13, LOW);//pump
-      digitalWrite(11, LOW);//left valve
-      delay(1000);
-      delay(first*1800); // dispense
-      digitalWrite(10, HIGH);
 
-      digitalWrite(12, LOW); //air valve
-      delay(5000); // clear water
-      digitalWrite(12, HIGH);
-
-      digitalWrite(10, LOW); //middle valve
-      delay(second*1400); // dispense
-      digitalWrite(10, HIGH);
-
-      digitalWrite(12, LOW); // air valve
-      delay(5000); // clear water
-      digitalWrite(12, HIGH);
-
-      digitalWrite(9, LOW); //right valve
-      delay(third*1400); // dispense
-      digitalWrite(9, HIGH);
-
-      digitalWrite(12, LOW); // air valve
-      delay(5000); // clear water
-      digitalWrite(13, HIGH);
-      digitalWrite(12, HIGH);
-    }
-    else if (first > 0 && second == 0 && third > 0) { //output from #1 and #3 drink
-      Serial.print("Drink 1, 3\n");
-      
-      digitalWrite(13, LOW);//pump
-      digitalWrite(11, LOW);//left valve
-      delay(1000);
-      delay(first*1800); // dispense
-      digitalWrite(10, HIGH);
-
-      digitalWrite(12, LOW); //air valve
-      delay(5000); // clear water
-      digitalWrite(12, HIGH);
-
-      digitalWrite(9, LOW); //right valve
-      delay(third*1400); // dispense
-      digitalWrite(9, HIGH);
-
-      digitalWrite(12, LOW); // air valve
-      delay(5000); // clear water
-      digitalWrite(13, HIGH);
-      digitalWrite(12, HIGH);
-    }
-    else if (first > 0 && second > 0 && third == 0) { //output from #1 and #2 drink
-      Serial.print("Drink 1, 2\n");
-      
-      digitalWrite(13, LOW);//pump
-      digitalWrite(11, LOW);//left valve
-      delay(1000);
-      delay(first*1800); // dispense
-      digitalWrite(10, HIGH);
-
-      digitalWrite(12, LOW); //air valve
-      delay(5000); // clear water
-      digitalWrite(12, HIGH);
-
-      digitalWrite(10, LOW); //middle valve
-      delay(second*1400); // dispense
-      digitalWrite(10, HIGH);
-
-      digitalWrite(12, LOW); // air valve
-      delay(5000); // clear water
-      digitalWrite(13, HIGH);
-      digitalWrite(12, HIGH);
-    }
-    else if (first == 0 && second > 0 && third > 0) { //output from #2 and #3 drink
-      Serial.print("Drink 2, 3\n");
-      
-      digitalWrite(13, LOW);//pump
-      digitalWrite(10, LOW); //middle valve
-      delay(1000);
-      delay(second*1800); // dispense
-      digitalWrite(10, HIGH);
-
-      digitalWrite(12, LOW); // air valve
-      delay(5000); // clear water
-      digitalWrite(12, HIGH);
-
-      digitalWrite(9, LOW); //right valve
-      delay(third*1400); // dispense
-      digitalWrite(9, HIGH);
-
-      digitalWrite(12, LOW); // air valve
-      delay(5000); // clear water
-      digitalWrite(13, HIGH);
-      digitalWrite(12, HIGH);
-    }
-    else if (first > 0 && second == 0 && third == 0) { //output from #1 drink
-      Serial.print("Drink 1\n");
-      
-      digitalWrite(13, LOW);//pump
-      digitalWrite(11, LOW);//left valve
-      delay(1500);
-      delay(first*1900); // dispense
-      digitalWrite(10, HIGH);
-
-      digitalWrite(12, LOW); // air valve
-      delay(5000); // clear water
-      digitalWrite(13, HIGH);
-      digitalWrite(12, HIGH);
-    }
-    else if (first == 0 && second > 0 && third == 0) { //output from #2 drink
-      Serial.print("Drink 2\n");
-      
-      digitalWrite(13, LOW);//pump
-      digitalWrite(10, LOW); //middle valve
-      delay(1500);
-      delay(second*1900); // dispense
-      digitalWrite(10, HIGH);
-      
-      digitalWrite(12, LOW); // air valve
-      delay(5000); // clear water
-      digitalWrite(13, HIGH);
-      digitalWrite(12, HIGH);
-    }
-    else if (first == 0 && second == 0 && third > 0) { //output from #3 drink
-      Serial.print("Drink 3\n");
-      
-      digitalWrite(13, LOW);//pump
-      digitalWrite(9, LOW); //right valve
-      delay(1500);
-      delay(third*1900); // dispense
-      digitalWrite(9, HIGH);
-
-      digitalWrite(12, LOW); // air valve
-      delay(5000); // clear water
-      digitalWrite(13, HIGH);
-      digitalWrite(12, HIGH);
-    }
+  //pinMode(11, OUTPUT);  // Valve 2 Drink 1
+  if (first > 0) { //output from drink 1
+    Serial.print("Drink 1 output\n");
+    Serial.print(first);
+    Serial.print(second);
+    Serial.print(third);
+    Serial.print('\n');
     
-//    if (data == '0') { 
-//      digitalWrite(13, LOW);//pump
-//      digitalWrite(10, LOW);//left valve
-//      delay(4900); // dispense
-//      digitalWrite(10, HIGH);
-//
-//      digitalWrite(12, LOW); //air valve
-//      delay(5000); // clear water
-//      digitalWrite(12, HIGH);
-//
-//      digitalWrite(9, LOW); //middle valve
-//      delay(5400); // dispense
-//      digitalWrite(9, HIGH);
-//
-//      digitalWrite(12, LOW); // air valve
-//      delay(5000); // clear water
-//      digitalWrite(12, HIGH);
-//
-////      digitalWrite(9, LOW); //right valve
-////      delay(6000); // dispense
-////      digitalWrite(9, HIGH);
-////
-////      digitalWrite(12, LOW); // air valve
-////      delay(5000); // clear water
-//      digitalWrite(13, HIGH);
-////      digitalWrite(12, HIGH);
-//    }
-//    if (data == '1' || input == "turn on") {          //Checks whether value of data is equal to 1
-//
-//      digitalWrite(13, LOW);
-//      digitalWrite(11, LOW);
-//      delay(3350);
-//      digitalWrite(11, HIGH);
-//      digitalWrite(12, LOW);
-//      delay(5000);
-//      digitalWrite(13, HIGH);
-//      digitalWrite(12, HIGH);
-//    }
-//    if (data == '2') {          //Checks whether value of data is equal to 1
-//      //      digitalWrite(13, HIGH);
-//      //      digitalWrite(7, HIGH);
-//      //      delay(6000); // dispense
-//      //      digitalWrite(7, LOW);
-//      //      digitalWrite(12, HIGH);
-//      //      delay(5000); // clear water
-//      //      digitalWrite(12, LOW);
-//      //      digitalWrite(10, HIGH);
-//      //      delay(7500); // dispense
-//      //      digitalWrite(10, LOW);
-//      //      digitalWrite(12, HIGH);
-//      //      delay(5000); // clear water
-//      //      digitalWrite(13, LOW);
-//      //      digitalWrite(12, LOW);
-//      digitalWrite(13, LOW);
-//      digitalWrite(10, LOW);
-//      delay(3300);
-//      digitalWrite(10, HIGH);
-//      digitalWrite(12, LOW);
-//      delay(5000);
-//      digitalWrite(13, HIGH);
-//      digitalWrite(12, HIGH);
-//    }
-//    if (data == '3') {          //Checks whether value of data is equal to 1
-//      //      digitalWrite(13, HIGH);
-//      //      digitalWrite(7, HIGH);
-//      //      delay(4000); // dispense
-//      //      digitalWrite(7, LOW);
-//      //      digitalWrite(12, HIGH);
-//      //      delay(5000); // clear water
-//      //      digitalWrite(12, LOW);
-//      //      digitalWrite(10, HIGH);
-//      //      delay(8000); // dispense
-//      //      digitalWrite(10, LOW);
-//      //      digitalWrite(12, HIGH);
-//      //      delay(5000); // clear water
-//      //      digitalWrite(13, LOW);
-//      //      digitalWrite(12, LOW);
-//      digitalWrite(13, LOW);
-//      digitalWrite(9, LOW);
-//      delay(3450);
-//      digitalWrite(9, HIGH);
-//      digitalWrite(12, LOW);
-//      delay(5000);
-//      digitalWrite(13, HIGH);
-//      digitalWrite(12, HIGH);
-//
-//    }
+    digitalWrite(13, LOW);//pump on
+    digitalWrite(11, LOW);//left valve on
+    delay(2000);
+    delay(first*1850); // dispense
+    digitalWrite(11, HIGH);//left valve off
+    
+    digitalWrite(12, LOW); // air valve
+    delay(5000); // clear water
+    digitalWrite(13, HIGH); //pump off
+    digitalWrite(12, HIGH);
+  }
+
+  //pinMode(10, OUTPUT); // Valve 3 Drink 2
+  if (second > 0) { //output from drink 2
+    Serial.print("Drink 2 output\n");
+    Serial.print(first);
+    Serial.print(second);
+    Serial.print(third);
+    Serial.print('\n');
+    
+    digitalWrite(13, LOW);//pump on
+    digitalWrite(10, LOW);//middle valve on
+    delay(2000);
+    delay(second*1850); // dispense
+    digitalWrite(10, HIGH);//middle valve off
+
+    digitalWrite(12, LOW); // air valve
+    delay(5000); // clear water
+    digitalWrite(13, HIGH); //pump off
+    digitalWrite(12, HIGH);
+  }
+
+  //pinMode(9, OUTPUT); // Valve 4 Drink 3
+  if (third > 0) { //output from drink 3
+    Serial.print("Drink 3 output\n");
+    Serial.print(first);
+    Serial.print(second);
+    Serial.print(third);
+    Serial.print('\n');
+    
+    digitalWrite(13, LOW);//pump on
+    digitalWrite(9, LOW);//right valve on
+    delay(2000);
+    delay(third*1850); // dispense
+    digitalWrite(9, HIGH);//right valve off
+
+    digitalWrite(12, LOW); // air valve
+    delay(5000); // clear water
+    digitalWrite(13, HIGH); //pump off
+    digitalWrite(12, HIGH);
+  }
+
+  data[0] = '0';
+  data[1] = '0';
+  data[2] = '0';
+  data[3] = '0';
+
 
   }
 
