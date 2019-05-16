6cbc3eb27f01ea0675abf5286519727d936c8691
Hector <hbara003@ucr.edu>
2019-05-15 19:11:32 -0700

    changed code to test SM on LCD

--- arduino_code/main/main.ino
+++ arduino_code/main/main.ino
@@ -1,89 +1,38 @@
 #include <SoftwareSerial.h>
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
 char data = 0;     //Variable for storing received data
 String input = "";
-SoftwareSerial EEBlue(3, 4);
+
 void setup()
 {         //Sets the data rate in bits per second (baud) for serial data transmission
-  EEBlue.begin(9600);
   pinMode(13, OUTPUT); // PUMP
   pinMode(12, OUTPUT); // Valve 1 uses air to push out remaining water
   pinMode(10, OUTPUT);  // Valve 2 Drink 1
   pinMode(7, OUTPUT); // Valve 3 Drink 2
   pinMode(11, OUTPUT); // Valve 4 Drink 3
+  // put your setup code here, to run once:
+  lcd.begin(16, 2);
+  lcd.print("smartMix");
 
 }
 
 // global
-int requestMade = -1;
+int requestMade = 1;
 // state machines
-enum PD_States {idle, pumpOn, p1, clear1, p2, clear2, p3, clear3, pumpOff} PD_State
+enum PD_States {idle, openValve, waitValve, nextValve} PD_State;
 void Tick_PourDrink();
 
 // preset drinks
 int drink1[3] = {1, 2, 3};
 int drink2[3] = {7000, 4000, 9000};
 int drink3[3] = {2000, 5000, 3000};
-int pourTimes[3][3] = {drink1, drink2, drink3}
+int pourTimes[3][3] = {*drink1, *drink2, *drink3};
 void loop()
 {
-  if (EEBlue.available() > 0) // Send data only when you receive data:
-  {
-    data = EEBlue.read();      //Read the incoming data and store it into variable data
-    input = EEBlue.readString();
-    EEBlue.print(data);        //Print Value inside data in Serial monitor
-    EEBlue.print("\n");        //New line
-    requestMade = data - '0';
-    if (data == '1' || input == "turn on") {          //Checks whether value of data is equal to 1
-      digitalWrite(13, HIGH);
-      digitalWrite(7, HIGH);
-      delay(5000); // dispense
-      digitalWrite(7, LOW);
-      digitalWrite(12, HIGH);
-      delay(5000); // clear water
-      digitalWrite(12, LOW);
-      digitalWrite(10, HIGH);
-      delay(7000); // dispense 
-      digitalWrite(10, LOW);
-      digitalWrite(12, HIGH);
-      delay(5000); // clear water
-      digitalWrite(13, LOW);
-      digitalWrite(12, LOW); 
-    }
-    if (data == '2') {          //Checks whether value of data is equal to 1
-      digitalWrite(13, HIGH);
-      digitalWrite(7, HIGH);
-      delay(6000); // dispense
-      digitalWrite(7, LOW);
-      digitalWrite(12, HIGH);
-      delay(5000); // clear water
-      digitalWrite(12, LOW);
-      digitalWrite(10, HIGH);
-      delay(7500); // dispense 
-      digitalWrite(10, LOW);
-      digitalWrite(12, HIGH);
-      delay(5000); // clear water
-      digitalWrite(13, LOW);
-      digitalWrite(12, LOW); 
-    }
-    if (data == '3') {          //Checks whether value of data is equal to 1
-      digitalWrite(13, HIGH);
-      digitalWrite(7, HIGH);
-      delay(4000); // dispense
-      digitalWrite(7, LOW);
-      digitalWrite(12, HIGH);
-      delay(5000); // clear water
-      digitalWrite(12, LOW);
-      digitalWrite(10, HIGH);
-      delay(8000); // dispense 
-      digitalWrite(10, LOW);
-      digitalWrite(12, HIGH);
-      delay(5000); // clear water
-      digitalWrite(13, LOW);
-      digitalWrite(12, LOW); 
-    }
-    
-  }
-
+  Tick_PourDrink();
 }
 
 void Tick_PourDrink() {
@@ -99,29 +48,31 @@ void Tick_PourDrink() {
                 PD_State = openValve;
                 // pump on
                 digitalWrite(13,LOW);
+                lcd.print("Pump On");
             }
             else {
                 PD_State = idle;
             }
             break;
         case openValve:
-            PD_State = valveWait;
+            PD_State = waitValve;
             break;
-        case valveWait:
+        case waitValve:
             if (cnt < pourTimes[requestMade][drindex]){
-                PD_State = valveWait;
+                PD_State = waitValve;
             }
             else {
                 PD_State = nextValve;
             }
             break;
         case nextValve:
-            if (drinex < 3) {
+            if (drindex < 3) {
                 PD_State = openValve;
             }
             else {
                 PD_State = idle;
                 // pump off
+                requestMade = -1;
                 digitalWrite(13,HIGH);
             }
             break;
@@ -129,24 +80,27 @@ void Tick_PourDrink() {
             PD_State = idle;
             break;
 	} // end transitions
-	switch (PD_States) { // actions
+	switch (PD_State) { // actions
 		case idle:
             break;
         case openValve:
-            if (drindex == 0 && ) {
+            if (drindex == 0) {
                 // open valve 1
                 digitalWrite(12,LOW);
+                lcd.print("Valve 1 Open");
             }
             else if (drindex == 1) {
                 // open valve 2
                 digitalWrite(10,LOW);
+                lcd.print("Valve 2 Open");
             }
             else if (drindex == 2) {
                 // open valve 3
-                digitalWrite(12,LOW);
+                digitalWrite(11,LOW);
+                lcd.print("Valve 3 Open");
             }
             break;
-        case valveWait:
+        case waitValve:
             ++cnt;
             break;
         case nextValve:
@@ -157,7 +111,7 @@ void Tick_PourDrink() {
             }
             else if (drindex == 1) {
                 // open valve 2
-                digitalWrite(9,HIGH)
+                digitalWrite(9,HIGH);
             }
             else if (drindex == 3) {
                 // open valve 3
