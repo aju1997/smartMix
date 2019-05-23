b5a52613ee894305d0d6df04ef5b0f0b5a8598c4
Hector Barajas <hbara003@ucr.edu>
2019-05-22 19:50:36 -0700

    converted main code to state machine

--- arduino_code/main/main.ino
+++ arduino_code/main/main.ino
@@ -3,11 +3,11 @@
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 String input = "";
-char data[4];
-//char data = 0;     //Variable for storing received data
+char drink[4];
+//char drink = 0;     //Variable for storing received data
 SoftwareSerial EEBlue(3, 4);
 void setup()
-{ //Sets the data rate in bits per second (baud) for serial data transmission
+{ //Sets the drink rate in bits per second (baud) for serial data transmission
   //  lcd.begin(6, 2);
   //  lcd.print("sma");
   EEBlue.begin(9600);
@@ -23,91 +23,156 @@ void setup()
   digitalWrite(10, HIGH); //middle(second) valve
   digitalWrite(9, HIGH);  // right(third) valve
 }
-
+// preset drinks
 int drink1[3] = {2000, 5000, 10000};
 int drink2[3] = {7000, 4000, 9000};
 int drink3[3] = {2000, 5000, 3000};
 int first = 0;
 int second = 0;
 int third = 0;
+
+// State Machines
+enum PD_States {PD_idle, PD_first, PD_second, PD_third, PD_prime, PD_dispense, PD_air} PD_State;
+void Tick_PourDrink();
+
+// main loop
 void loop()
 {
-  if (EEBlue.available() > 0) // Send data only when you receive data:
+  if (EEBlue.available() > 0) // Send drink only when you receive data:
   {
-    input = EEBlue.readString(); //Read the incoming data and store it into variable data
+    input = EEBlue.readString(); //Read the incoming drink and store it into variable data
     
-    input.toCharArray(data,4);\
+    input.toCharArray(drink,4);
     
-    first = data[0] - '0';
-    second = data[1] - '0';
-    third = data[2] - '0';
+    // grab input
+    first = drink[0] - '0';
+    second = drink[1] - '0';
+    third = drink[2] - '0';
 
-  //pinMode(11, OUTPUT);  // Valve 2 Drink 1
-  if (first > 0) { //output from drink 1
-    Serial.print("Drink 1 output\n");
-    Serial.print(first);
-    Serial.print(second);
-    Serial.print(third);
-    Serial.print('\n');
-    
-    digitalWrite(13, LOW);//pump on
-    digitalWrite(11, LOW);//left valve on
-    delay(2000);
-    delay(first*1850); // dispense
-    digitalWrite(11, HIGH);//left valve off
-    
-    digitalWrite(12, LOW); // air valve
-    delay(5000); // clear water
-    digitalWrite(13, HIGH); //pump off
-    digitalWrite(12, HIGH);
+    // use input
+    Tick_PourDrink();
+
+    // clear input
+    drink[0] = '0';
+    drink[1] = '0';
+    drink[2] = '0';
+    drink[3] = '0';
   }
+}
 
-  //pinMode(10, OUTPUT); // Valve 3 Drink 2
-  if (second > 0) { //output from drink 2
-    Serial.print("Drink 2 output\n");
-    Serial.print(first);
-    Serial.print(second);
-    Serial.print(third);
-    Serial.print('\n');
-    
-    digitalWrite(13, LOW);//pump on
-    digitalWrite(10, LOW);//middle valve on
-    delay(2000);
-    delay(second*1850); // dispense
-    digitalWrite(10, HIGH);//middle valve off
+void Tick_PourDrink() {
+    static int cnt = 0;
+    static int drindex = 0;
 
-    digitalWrite(12, LOW); // air valve
-    delay(5000); // clear water
-    digitalWrite(13, HIGH); //pump off
-    digitalWrite(12, HIGH);
-  }
+    // transitions
+    switch (PD_State) {
+        case PD_idle:
+            if (first || second || third) {
+                PD_State = PD_first;
+            }
+            else {
+                PD_State = PD_idle;
+            }
+            break;
+        case PD_first:
+            if (first) {
+                digitalWrite(11, LOW); // first valve open
+                digitalWrite(13, LOW); // pump on
+                drindex = 0;
+                PD_State = PD_prime; // prime, dispense, clear tubes
+            }
+            else {
+                PD_State = PD_second;
+            }
+            break;
+        case PD_second:
+            if (second) {
+                digitalWrite(10, LOW); // second valve open
+                digitalWrite(13, LOW); // pump on
+                drindex = 1;
+                PD_State = PD_prime; // prime, dispense, clear tubes
+            }
+            else {
+                PD_State = PD_third;
+            }
+            break;
+        case PD_third:
+            if (third) {
+                digitalWrite(9, LOW);  // third valve open
+                digitalWrite(13, LOW); // pump on
+                drindex = 2;
+                PD_State = PD_prime; // prime, dispense, clear tubes
+            }
+            else {
+                PD_State = PD_idle;
+            }
+            break;
+        case PD_prime:
+            if (cnt < 2000) {
+                PD_State = PD_prime;
+            }
+            else {
+                PD_State = PD_dispense;
+                cnt = 0;
+            }
+            break;
+        case PD_dispense:
+            if (cnt < 1850*drink[drindex]) {
+                PD_State = PD_dispense;
+            }
+            else {
+                PD_State = PD_air;
+                // close all valves
+                digitalWrite(11,HIGH);
+                digitalWrite(10,HIGH);
+                digitalWrite(9,HIGH);
+                // open air valve
+                digitalWrite(12, LOW);
+                // reset counter
+                cnt = 0;
+            }
+            break;
+        case PD_air:
+            if (cnt < 5000) {
+                PD_State = PD_air;
+            }
+            else {
+                cnt = 0;
 
-  //pinMode(9, OUTPUT); // Valve 4 Drink 3
-  if (third > 0) { //output from drink 3
-    Serial.print("Drink 3 output\n");
-    Serial.print(first);
-    Serial.print(second);
-    Serial.print(third);
-    Serial.print('\n');
-    
-    digitalWrite(13, LOW);//pump on
-    digitalWrite(9, LOW);//right valve on
-    delay(2000);
-    delay(third*1850); // dispense
-    digitalWrite(9, HIGH);//right valve off
-
-    digitalWrite(12, LOW); // air valve
-    delay(5000); // clear water
-    digitalWrite(13, HIGH); //pump off
-    digitalWrite(12, HIGH);
-  }
-
-  data[0] = '0';
-  data[1] = '0';
-  data[2] = '0';
-  data[3] = '0';
-
-
-  }
-
+                digitalWrite(13, HIGH); // turn off pump
+                digitalWrite(12, HIGH); // close air valve
+                PD_State = (second) ? PD_second : PD_third;
+            }
+            break;
+        default:
+            PD_State = PD_idle;
+            break;
+    }
+    // actions
+    switch (PD_State) {
+        case PD_idle:
+            // be idle
+            break;
+        case PD_first:
+            drindex = 0;
+            break;
+        case PD_second:
+            drindex = 1;
+            break;
+        case PD_third:
+            drindex = 2;
+            break;
+        case PD_prime:
+            ++cnt;
+            break;
+        case PD_dispense:
+            ++cnt;
+            break;
+        case PD_air:
+            ++cnt;
+            break;
+        default:
+            // do nothing
+            break;
+    }
 }
