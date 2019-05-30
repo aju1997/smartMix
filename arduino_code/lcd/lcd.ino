cf029d5981fd9c2006d7276f01bfcbf309790f4e
Hector <hbara003@ucr.edu>
2019-05-29 18:17:41 -0700

    added joystick, menu, get input functions/SMs

--- arduino_code/lcd/lcd.ino
+++ arduino_code/lcd/lcd.ino
@@ -1,14 +1,125 @@
-#include <LiquidCrystal.h>
+#include "Menus.h"
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
   // put your setup code here, to run once:
   lcd.begin(16, 2);
-  lcd.print("smartMix");
+  pinMode(6,INPUT);
+}
+#define NONE 0
+#define UP 1
+#define DWN 2
+// gets input from joystick (analog) and button (pin)
+int getInput();
+// sets position of cursor using joystick input
+enum CS_States {none, up, down} CS_State;
+void Tick_Joystick();
+// navigates menu options
+enum M_States {M_init, main, select, create, presets, customs} M_State;
+void Tick_Menu();
+
+char cursorPos = 0;
+char buttonPress = 0;
+void loop() {
+    buttonPress = digitalRead(6);
+    delay(1);
+    Tick_Menu();
+    Tick_Joystick();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void Tick_Menu() {
+  // transitions
+  switch (M_State) {
+    case M_init:
+      M_State = main;
+      mainMenu();
+      break;
+    case main:
+      if (buttonPress && cursorPos) {
+        M_State = create;
+      }
+      else if (buttonPress && !cursorPos)  {
+        M_State = select;
+      }
+      else {
+        M_State = main;
+      }
+      break;
+    case create:
+      break;
+    default:
+      M_State = M_init;
+      break;
+  }
+  // actions
+  switch (M_State) {
+    case main:
+      break;
+    case select:
+      selectMenu();
+      break;
+    case create:
+      createMenu(1);
+      break;
+    default:
+      break;
+  }
+}
 
+
+int getInput() {
+  if (analogRead(A1) > 550) {
+    return UP;
+  }
+  else if ( analogRead(A1) < 450) {
+    return DWN;
+  }
+  else {
+    return 0;
+  }
+}
+void Tick_Joystick() {
+  // transitions
+  switch (CS_State) {
+    case none:
+      if (getInput() == UP) {
+        CS_State = up;
+      }
+      else if (getInput() == DWN) {
+        CS_State = down;
+      }
+      else {
+        CS_State = none;
+      }
+      break;
+    case up:
+      CS_State = none;
+      break;
+    case down:
+      CS_State = none;
+      break;
+    default:
+      CS_State = none;
+      break;
+      
+  }
+  // actions
+  switch (CS_State) {
+    case up:
+      lcd.setCursor(13,0);
+      lcd.print("*");
+      lcd.setCursor(13,1);
+      lcd.print(" ");
+      cursorPos = 0;
+      break;
+    case down:
+      lcd.setCursor(13,0);
+      lcd.print(" ");
+      lcd.setCursor(13,1);
+      lcd.print("*");
+      cursorPos = 1;
+      break;
+    default:
+      break;
+  }
 }
