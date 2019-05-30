091a37a6cfcbc73e2b7c5a0d797b3371be1aef5b
Anderson Ju <aju001@ucr.edu>
2019-05-30 01:28:42 -0700

    More LCD function and Menu additions

--- arduino_code/lcd/Menus.h
+++ arduino_code/lcd/Menus.h
@@ -3,21 +3,23 @@
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void mainMenu() {
+  lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Select    ");
   lcd.setCursor(0, 1);
   lcd.print("Create    ");
 }
 
-void selectMenu() {
+void chooseMenu() {
+  lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Presets");
   lcd.setCursor(0, 1);
   lcd.print("Customs");
 }
 
-void createMenu(char drink, char amount) {
-  drink += '0';
+void createMenu(int drink, char amount) {
+  //drink += '0';
   amount += '0';
   lcd.setCursor(0, 0);
   // prints drink# currently being edited
@@ -28,4 +30,44 @@ void createMenu(char drink, char amount) {
   lcd.print("ounces: ");
   lcd.print(amount);
   lcd.print("         "); // clear the cursor "*"
+}
+
+void pourStoreMenu() {
+	lcd.clear();
+	lcd.setCursor(0, 0);
+	lcd.print("Pour");
+	lcd.setCursor(0, 1);
+	lcd.print("Store");
+}
+
+void confirmDrinkMenu() {
+	lcd.clear();
+	lcd.setCursor(0, 0);
+	lcd.print("Confirm? Yes");
+	lcd.setCursor(9, 1);
+	lcd.print("No");
+}
+
+void storeConfirmMenu() {
+	lcd.clear();
+	lcd.setCursor(0, 0);
+	lcd.print("Confirm? Yes");
+	lcd.setCursor(9, 1);
+	lcd.print("No");
+}
+
+void storeDrinkYesMenu() {
+	lcd.clear();
+	lcd.setCursor(0, 0);
+	lcd.print("Your drink has");
+	lcd.setCursor(0, 1);
+	lcd.print("been stored!");
+}
+
+void pouringDrinkYesMenu() {
+	lcd.clear();
+	lcd.setCursor(0, 0);
+	lcd.print("Your drink is");
+	lcd.setCursor(0, 1);
+	lcd.print("pouring!");
 }
--- arduino_code/lcd/lcd.ino
+++ arduino_code/lcd/lcd.ino
@@ -1,10 +1,23 @@
 #include "Menus.h"
 
+byte arrowCursor[8] = {
+  B00010,
+  B00110,
+  B01110,
+  B11110,
+  B01110,
+  B00110,
+  B00010,
+  B00000
+};
 
 void setup() {
   // put your setup code here, to run once:
+  lcd.createChar(0, arrowCursor);
   lcd.begin(16, 2);
+  lcd.write(byte(0));
   pinMode(6,INPUT);
+  Serial.begin(9600);
 }
 
 #define NONE 0
@@ -19,16 +32,35 @@ enum CP_States {none, up, down} CP_State;
 void Tick_CursorPos();
 
 // navigates menu options
-enum M_States {M_init, main, select, create, presets, customs} M_State;
+enum M_States {M_init, main, choose, create, presets, customs, pourStore, confirmDrink, store, storeConfirm, yesDrink} M_State;
 void Tick_Menu();
 
 // increment and decrement a value with joystick (used for menu navigation)
 enum ID_States {idle, increment, waitRelease, decrement} ID_State;
 void Tick_IncDec();
-char value = 0;
+int value = 0;
+String data = "0";
+
+enum Button_States {low, high, wait} Button_State;
+void Tick_Button();
 
 char cursorPos = 0;
 char buttonPress = 0;
+char buttonHold = 0;
+
+void cursor() {
+  lcd.setCursor(13,0);
+    if (!cursorPos)
+      lcd.write(byte(0));
+    else
+      lcd.print(" ");
+    lcd.setCursor(13,1);
+    if (!cursorPos)
+      lcd.print(" ");
+    else
+      lcd.write(byte(0));
+}
+
 void loop() {
     buttonPress = digitalRead(6);
 
@@ -36,6 +68,7 @@ void loop() {
     Tick_Menu();
     Tick_IncDec();
     Tick_CursorPos();
+    Tick_Button();
 }
 
 /* inputs: gets joystick up/down input position
@@ -92,7 +125,9 @@ void Tick_IncDec() {
  * return: prints current menu screen from Menus.h (most LCD writing done here)
  */
 void Tick_Menu() {
-  static char drindex = 0;
+  static int drindex = 0;
+  static int storeCounter = 0;
+  static int yesDrinkCounter = 0;
   // transitions
   switch (M_State) {
     case M_init:
@@ -101,24 +136,85 @@ void Tick_Menu() {
       break;
     case main:
       // uses input from Tick_CursorPos
-      if (buttonPress && cursorPos) {
+      if (cursorPos && !buttonHold && buttonPress) { //create state
         M_State = create;
-        createMenu(1, 0);
+        drindex = 0;
+        createMenu(drindex, 0);
       }
-      else if (buttonPress && !cursorPos)  {
-        M_State = select;
-        selectMenu();
+      else if (!cursorPos && !buttonHold && buttonPress)  { //select state
+        M_State = choose;
+        chooseMenu();
       }
       else {
         M_State = main;
       }
       break;
     case create:
-      if (drindex < 3) {
+      if (drindex < 4) { //once 3 drinks are done
           M_State = create;
       }
       else {
-          M_State = main;
+          M_State = pourStore;
+          pourStoreMenu();
+      }
+      break;
+    case pourStore:
+      if (cursorPos && !buttonHold && buttonPress) {
+        M_State = storeConfirm;
+        storeConfirmMenu();
+      }
+      else if (!cursorPos && !buttonHold && buttonPress) {
+        M_State = confirmDrink;
+        confirmDrinkMenu();
+      }
+      else {
+        M_State = pourStore;
+      }
+      break;
+    case confirmDrink:
+      if (!cursorPos && !buttonHold && buttonPress) {
+        M_State = yesDrink;
+        pouringDrinkYesMenu();
+      }
+      else if (cursorPos && !buttonHold && buttonPress) {
+        M_State = main;
+        mainMenu();
+      }
+      else {
+        M_State = confirmDrink;
+      }
+      break;
+    case store:
+      if (storeCounter < 1500) {
+        M_State = store;
+      }
+      else {
+        storeCounter = 0;
+        M_State = main;
+        mainMenu();
+      }
+      break;
+    case storeConfirm:
+      if (!cursorPos && !buttonHold && buttonPress) {
+        M_State = store;
+        storeDrinkYesMenu();
+      }
+      else if (cursorPos && !buttonHold && buttonPress) {
+        M_State = main;
+        mainMenu();
+      }
+      else {
+        M_State = storeConfirm;
+      }
+      break;
+    case yesDrink:
+      if (yesDrinkCounter < 2500) {
+        M_State = yesDrink;
+      }
+      else {
+        yesDrinkCounter = 0;
+        M_State = main;
+        mainMenu();
       }
       break;
     default:
@@ -129,23 +225,12 @@ void Tick_Menu() {
   switch (M_State) {
     case main:
       // print cursor
-      lcd.setCursor(13,0);
-      if (!cursorPos)
-        lcd.print("*");
-      else
-        lcd.print(" ");
-      lcd.setCursor(13,1);
-      if (!cursorPos)
-        lcd.print(" ");
-      else
-        lcd.print("*");
+      cursor();
       break;
-    case select:
+    case choose: //still need to do
       break;
     case create:
-      if (buttonPress) {
-          // FIXME: button press captured too fast,
-          //        increment is buggy (make SM for button?)
+      if (!buttonHold && buttonPress) {
           ++drindex;
           value = 0;
       }
@@ -156,7 +241,35 @@ void Tick_Menu() {
       else if (value <= 0) {
         value = 0;
       }
-      createMenu(0, value);
+      //add if statements to store the value of what the user specified
+      
+//      if (drindex == 1 && data.length() == 1 && data[0] != value) {
+//        data[0] = String(value);
+//        Serial.print(data);
+//      }
+//      if (drindex == 2) {
+//        data += String(value);
+//      }
+//      if (drindex == 3) {
+//        data += String(value);
+//      }
+      //Serial.print(data);
+      createMenu(drindex, value);
+      break;
+    case pourStore:
+      cursor();
+      break;
+    case confirmDrink:
+      cursor();
+      break;
+    case store:
+      ++storeCounter;
+      break;
+    case storeConfirm:
+      cursor();
+      break;
+    case yesDrink:
+      ++yesDrinkCounter;
       break;
     default:
       break;
@@ -185,4 +298,43 @@ void Tick_CursorPos() {
         cursorPos = 0;
     else if (getYinput() == DWN)
         cursorPos = 1;
+}
+
+void Tick_Button() {
+   switch (Button_State) { //Transitions
+        case low:
+            if (buttonPress) {
+              Button_State = high;
+            }
+            else {
+              Button_State = low;
+            }
+            break;
+        case high:
+            Button_State = wait;
+            break;
+        case wait:
+            if (buttonPress) {
+              Button_State = wait;
+            }
+            else {
+              Button_State = low;
+            }
+            break;
+        default:
+            Button_State = low;
+            break;
+    }
+    switch (Button_State) { //Actions
+        case low:
+            buttonHold = 0; //not holding
+            break;
+        case high:
+            buttonHold = 1; //holding
+            break;
+        case wait:
+            break;
+        default:
+            break;
+    }
 }
